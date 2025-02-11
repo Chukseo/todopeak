bc2f486c6f94f23e31d6705b9172e8570c518978
Chukseo <164390660+Chukseo@users.noreply.github.com>
2025-02-11 14:42:21 +0100

    models

--- todoapis/models.py
+++ todoapis/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    complete = models.BooleanField(default=False)
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+   
